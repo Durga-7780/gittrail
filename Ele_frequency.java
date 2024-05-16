e0e4f5a8f766fccdbd4c6c27693fbaf655a6d227
Durga-7780 <167679258+Durga-7780@users.noreply.github.com>
2024-05-16 12:41:14 +0530

    Delete Ele_frequency.java

--- Ele_frequency.java
+++
@@ -1,9 +0,0 @@
-import java.util.stream.*;
-import java.util.*;
-class Ele_frequency{
-  public static void main(String as[])
-  {
-    int a[]={10,2,10,5,10,3,1,2,10,5};
-    Arrays.stream(a).boxed().collect(Collectors.groupingBy(x->x,LinkedHashMap::new,Collectors.counting())).entrySet().stream().map(x->x).forEach(System.out::println);
-}
-}
