4ad5086a13bbb3f39971e3a4755a42cdbe229830
durga <durgapragada204@gmail.com>
2024-05-16 11:43:47 +0530

    commited syntax error

--- Ele_frequency.java
+++ Ele_frequency.java
@@ -4,6 +4,6 @@ class Ele_frequency{
   public static void main(String as[])
   {
     int a[]={10,2,10,5,10,3,1,2,10,5};
-    Arrays.stream(a).boxed().collect(Collectors.groupingBy(x->x,LinkedHashMap::new,Collectors.counting()).entrySet().stream().map(x->x.getKey()).forEach(x->System.out.println(x)));
-  }
+    Arrays.stream(a).boxed().collect(Collectors.groupingBy(x->x,LinkedHashMap::new,Collectors.counting())).entrySet().stream().map(x->x).forEach(System.out::println);
+}
 }
